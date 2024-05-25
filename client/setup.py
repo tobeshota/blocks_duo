f0e0c99bb13987eefc6735697b571ef4bfd2ef26
toshota <toshota@student.42tokyo.jp>
2024-05-25 18:24:12 +0900

    [fix] fix `setup.py`

--- client/setup.py
+++ client/setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as requirements_file:
     install_requirements = requirements_file.read().splitlines()
 setup(
-    name="blocks_duo_ss_tarou",
+    name="blocks_duo_atami_drive",
     version="0.0.1",
-    description="smartscape blocks-duo player package",
-    author="Yoshitomi",
+    description="blocks-duo player package",
+    author="atami_drive",
     packages=find_packages(),
     install_requires=install_requirements,
     entry_points={
         "console_scripts": [
-            "ss_tarou=ss_player.main:main",
+            "atami_drive=ss_player.main:main",
         ]
     },
     classifiers=[
